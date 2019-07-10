7ac8197dea58736552169b82424ece2541ba3079
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-10 16:52:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray 
+function addElementToBeginningOfArray (array, element) {
+  return ()
+}
 const addElementToBeginningOfArray =["foo", ...array]
