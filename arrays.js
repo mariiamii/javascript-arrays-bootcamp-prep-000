515fffd5e16f4e66ac3c2e58cedeb6dedc154d91
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-10 19:24:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,6 +32,12 @@ function removeElementFromBeginningOfArray (array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
-  return array.pop()
+ array.pop()
+  return array
 }
+
+
+
+
+
 
