e76e2c4c6950b9f51eebefdbf797b50210de98e0
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-10 17:22:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,66 @@ function destructivelyAddElementToEndOfArray (array, element) {
   return array 
 }
 
+function accessElementInArray (array, index) {
+  
+}
+
+
+
+
+  describe('accessElementInArray(array, index)', () => {
+    it('accesses the element in array at the given index', () => {
+      expect(accessElementInArray([1, 2, 3], 2)).to.equal(3)
+    })
+  })
+  
+  describe('destructivelyRemoveElementFromBeginningOfArray(array)', ()=>{
+    it('returns the array with the first element removed', () => {
+      expect(destructivelyRemoveElementFromBeginningOfArray([1, 2, 3])).to.eql([2, 3])
+    })
+    
+    it('alters the original array', ()=>{
+      const array = [1, 2, 3];
+      destructivelyRemoveElementFromBeginningOfArray(array);
+      expect(array).to.eql([2, 3]);
+    })
+  })
+
+  describe('removeElementFromBeginningOfArray(array)', () => {
+    it('removes the first element from the array', () => {
+      expect(removeElementFromBeginningOfArray([1, 2, 3])).to.eql([2, 3])
+    })
+
+    it('does not alter the original array', () => {
+      const array = [1, 2, 3];
+
+      removeElementFromBeginningOfArray(array);
+
+      expect(array).to.eql([1, 2, 3]);
+    })
+  })
+
+  describe('destructivelyRemoveElementFromEndOfArray(array)', () => {
+    it('returns the array with the last element removed', () => {
+      expect(destructivelyRemoveElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('alters the original array', ()=>{
+      const array = [1, 2, 3];
+      destructivelyRemoveElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2]);
+    })
+  })
+  
+  describe('removeElementFromEndOfArray(array)', () => {
+    it('removes the last element from the array', () => {
+      expect(removeElementFromEndOfArray([1, 2, 3])).to.eql([1, 2])
+    })
+    
+    it('does not alter the original array', () => {
+      const array = [1, 2, 3];
+      removeElementFromEndOfArray(array);
+      expect(array).to.eql([1, 2, 3]);
+    })
+  })
+})
