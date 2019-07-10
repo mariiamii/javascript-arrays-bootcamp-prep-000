e5917fc1411b0d0cf91bcb94ace7206544500a30
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-10 17:08:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,12 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
   return array 
 }
 
+function addElementToEndOfArray (array, element) {
+  return [...array, element] 
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array 
+}
+
